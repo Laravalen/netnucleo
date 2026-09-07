8ca423f9473578bc6e13d99f7d51aa70fb063a49
Laravalen <0001055622@senaimgaluno.com.br>
2026-09-07 13:52:13 -0300

    Delete sistema_gestao_academica directory

--- sistema_gestao_academica/partials/header.php
+++
@@ -1,2 +0,0 @@
-<?php require_once __DIR__.'/../functions.php'; ?>
-<!doctype html><html lang="pt-BR"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title><?=e(page_title($title ?? 'Gestão Acadêmica'))?></title><link rel="stylesheet" href="assets/css/style.css"><script defer src="assets/js/app.js"></script></head><body><div class="app">
